af97565e3d46783129463f07550b3ed43825cccc
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-10-25 09:55:10 +0100

    widening search box, few font-sizes and making comments RTE bg white

--- drupal/sites/all/themes/sevencivi/page.tpl.php
+++ drupal/sites/all/themes/sevencivi/page.tpl.php
@@ -13,27 +13,27 @@
   <div id="page">
     <?php print render($secondary_local_tasks); ?>
 
-
-    <div id="content" class="clearfix">
-      <div class="element-invisible"><a id="main-content"></a></div>
-      <?php if ($messages): ?>
-        <div id="console" class="clearfix"><?php print $messages; ?></div>
-      <?php endif; ?>
-      <?php if ($page['help']): ?>
-        <div id="help">
-          <?php print render($page['help']); ?>
-        </div>
-      <?php endif; ?>
-      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-      <?php print render($page['content']); ?>
-    </div>
-
-    <?php if ($page['right_sidebar']): ?>
+	<?php if ($page['right_sidebar']): ?>
         <div id="right_sidebar">
           <?php print render($page['right_sidebar']); ?>
         </div>
     <?php endif; ?>
-
+	
+	<div id="contentWrap">
+    	<div id="content" class="clearfix">
+	      <div class="element-invisible"><a id="main-content"></a></div>
+	      <?php if ($messages): ?>
+	        <div id="console" class="clearfix"><?php print $messages; ?></div>
+	      <?php endif; ?>
+	      <?php if ($page['help']): ?>
+	        <div id="help">
+	          <?php print render($page['help']); ?>
+	        </div>
+	      <?php endif; ?>
+	      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+	      <?php print render($page['content']); ?>
+	    </div>
+	</div>
     <div id="footer">
       <?php print $feed_icons; ?>
     </div>
